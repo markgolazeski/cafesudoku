77aee66f9f7a998c0c3c1fb641e9d1ff4e5048dc
jgm564 <jgm564@5828094f-9d29-0410-bcad-4118a2cb9d5c>
2007-03-04 18:09:00 +0000

    Changed 0 to _ in ComboOptions

--- project/Cell.java
+++ project/Cell.java
@@ -47,6 +47,9 @@ public class Cell {
 			choices.add(i);
 		}
 		_dropDownList = new JComboBox(choices);
+		this._dropDownList.removeItem(0);
+		this._dropDownList.insertItemAt(new String("_"),0);
+		this._dropDownList.setSelectedIndex(0);
 		
 	}
 	
